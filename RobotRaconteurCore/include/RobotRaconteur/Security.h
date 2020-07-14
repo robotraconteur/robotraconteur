572f18bc79796a31141dc1980a18175a1f107564
John Wason <wason@wasontech.com>
2020-07-14 18:38:54 -0400

    Doxygen Security.h

--- RobotRaconteurCore/include/RobotRaconteur/Security.h
+++ RobotRaconteurCore/include/RobotRaconteur/Security.h
@@ -30,18 +30,58 @@ namespace RobotRaconteur
 	class ROBOTRACONTEUR_CORE_API  UserAuthenticator;
 	class ROBOTRACONTEUR_CORE_API  ServerContext;
 
+	/**
+	 * @brief Security policy for Robot Raconteur service
+	 * 
+	 * The security policy sets an authenticator, and a set of policies. PasswordFileUserAuthenticator is
+	 * an example of an authenticator. The valid options for Policies are as follows:
+	 * 
+	 * | Policy name | Possible Values | Default | Description |
+	 * | ---         | ---             | ---      | ---          |
+	 * | requirevaliduser | true,false | false | Set to "true" to require a user be authenticated before accessing service |
+	 * | allowobjectlock | true,false | false | If "true" allow users to request object locks. requirevaliduser most also be "true" |
+	 * 
+	 * The security policy is passed as a paremeter to 
+	 * RobotRaconteurNode::RegisterService(boost::string_ref, boost::string_ref, 
+	 * boost::shared_ptr<RRObject>,  boost::shared_ptr< ServiceSecurityPolicy > securitypolicy),
+	 * or set using ServerContext::SetSecurityPolicy().
+	 * 
+	 * See \ref security for more information.
+	 * 
+	 */
 	class ROBOTRACONTEUR_CORE_API  ServiceSecurityPolicy
 	{
 	public:
 		RR_SHARED_PTR<UserAuthenticator> Authenticator;
 		std::map<std::string, std::string> Policies;
 
+		/**
+		 * @brief Construct an empty ServiceSecurityPolicy
+		 * 
+		 * Must use boost::make_shared<ServiceSecurityPolicy>()
+		 * 
+		 */
 		ServiceSecurityPolicy();
 
+		/**
+		 * @brief Construct a ServiceSecurityPolicy
+		 * 
+		 * @param Authenticator The user authenticator
+		 * @param Policies The security policies
+		 */
 		ServiceSecurityPolicy(RR_SHARED_PTR<UserAuthenticator> Authenticator, const std::map<std::string, std::string>& Policies);
 
 	};
 	
+	/**
+	 * @brief Class representing an authenticated user
+	 * 
+	 * Use ServerEndpoint::GetCurrentAuthenticatedUser() to retrieve the
+	 * authenticated user making a request
+	 * 
+	 * See \ref security for more information.
+	 * 
+	 */
 	class ROBOTRACONTEUR_CORE_API  AuthenticatedUser
 	{
 	private:
@@ -56,18 +96,41 @@ namespace RobotRaconteur
 		RR_WEAK_PTR<ServerContext> context;
 
 	public:
+		/** @brief The authenticated username */
 		virtual std::string GetUsername();
 
+		/** @brief The user privileges */
 		virtual std::vector<std::string> GetPrivileges();
 
+		/** @brief The user properties */
 		virtual std::vector<std::string> GetProperties();
 
+		/** @brief The user login time */
 		virtual boost::posix_time::ptime GetLoginTime();
 
+		/** @brief The user last access time */
 		virtual boost::posix_time::ptime GetLastAccessTime();
 
+		/**
+		 * @brief Construct a new AuthenticatedUser
+		 * 
+		 * Must use boost::make_shared<AuthenticatedUser>()
+		 * 
+		 * Valid privileges are as follows:
+		 * 
+		 * | Privilege Name | Description |
+		 * | --- | --- |
+		 * | objectlock | Allow user to lock objects |
+		 * | objectlockoverride | Allow user to unlock object locks made by other users |
+		 * 
+		 * @param username The username
+		 * @param privileges The user privileges
+		 * @param properties The user properties
+		 * @param context The context of the service
+		 */
 		AuthenticatedUser(boost::string_ref username, const std::vector<std::string>& privileges, const std::vector<std::string>& properties, RR_SHARED_PTR<ServerContext> context);
 
+		/** @brief Update the last access time to now */
 		virtual void UpdateLastAccess();
 
 		virtual ~AuthenticatedUser() {}
@@ -75,15 +138,48 @@ namespace RobotRaconteur
 	};
 
 
+	/**
+	 * @brief Base class for user authenticators
+	 * 
+	 * Used with ServiceSecurityPolicy to secure services
+	 * 
+	 * Override AuthenticateUser to implement different types
+	 * of user authenticators.
+	 * 
+	 */
 	class ROBOTRACONTEUR_CORE_API  UserAuthenticator
 	{
 	public:
+		/**
+		 * @brief Authenticate a user using username and credentials
+		 * 
+		 * The service will call AuthenticateUser when clients attempt to authenticate. The
+		 * username and credentials are passed to RobotRaconteurNode::ConnectService() or
+		 * RobotRaconteurNode::AsyncConnectService(). The authenticator must analyze these
+		 * values to decide how to authenticate the user.
+		 * 
+		 * On successful authentication, return a populated AuthenticatedUser object. On
+		 * failure, throw an exception.
+		 * 
+		 * See \ref security for more information.
+		 * 
+		 * @param username The username provided by the client
+		 * @param credentials The credentials provided by the client
+		 * @param context The context of the service requesting authentication
+		 * @return RR_SHARED_PTR<AuthenticatedUser> An authenticated user object
+		 */
 		virtual RR_SHARED_PTR<AuthenticatedUser> AuthenticateUser(boost::string_ref username, const std::map<std::string, RR_INTRUSIVE_PTR<RRValue> > &credentials, RR_SHARED_PTR<ServerContext> context) = 0;
 
 		virtual ~UserAuthenticator() {}
 
 	};
 
+	/**
+	 * @brief Simple authenticator using a list of username, password hash, and privileges stored in a file or string
+	 * 
+	 * See \ref security for more information.
+	 * 
+	 */
 	class ROBOTRACONTEUR_CORE_API  PasswordFileUserAuthenticator : public UserAuthenticator
 	{
 
@@ -101,8 +197,18 @@ namespace RobotRaconteur
 		std::map<std::string, RR_SHARED_PTR<User> > validusers;
 
 	public:
+		/**
+		 * @brief Construct a new PasswordUserAuthenticator using text supplied as a stream
+		 * 
+		 * @param file The file text as a stream
+		 */
 		PasswordFileUserAuthenticator(std::istream &file);
 
+		/**
+		 * @brief Construct a new PasswordUserAuthenticator using text supplied as a string
+		 * 
+		 * @param data The file text
+		 */
 		PasswordFileUserAuthenticator(boost::string_ref data);
 
 		virtual ~PasswordFileUserAuthenticator() {}
@@ -122,9 +228,13 @@ namespace RobotRaconteur
 	};
 
 #ifndef BOOST_NO_CXX11_TEMPLATE_ALIASES
+	/** @brief Convenience alias for ServiceSecurityPolicy shared_ptr */
 	using ServiceSecurityPolicyPtr = RR_SHARED_PTR<ServiceSecurityPolicy>;
+	/** @brief Convenience alias for AuthenticatedUser shared_ptr */
 	using AuthenticatedUserPtr = RR_SHARED_PTR<AuthenticatedUser>;
+	/** @brief Convenience alias for UserAuthenticator shared_ptr */
 	using UserAuthenticatorPtr = RR_SHARED_PTR<UserAuthenticator>;
+	/** @brief Convenience alias for PasswordFileUserAuthenticator shared_ptr */
 	using PasswordFileUserAuthenticatorPtr = RR_SHARED_PTR<PasswordFileUserAuthenticator>;
 #endif	
 
