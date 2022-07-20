74506161764b6f43c4c7ccb97d97b5d06843b911
John Wason <wason@wasontech.com>
2022-07-19 22:07:01 -0400

    Initial work on node directories

---
+++ RobotRaconteurCore/src/NodeDirectories.cpp
@@ -0,0 +1,10 @@
+#include "RobotRaconteur/NodeDirectories.h"
+
+namespace RobotRaconteur
+{
+
+    NodeDirectories GetDefaultNodeDirectories(RR_SHARED_PTR<RobotRaconteurNode> node, bool create_user_dirs)
+    {
+        
+    }
+}
