36515ef52b5df1f0ed72b266afd51384dfef08ed
John Wason <wason@wasontech.com>
2022-07-20 02:57:07 -0400

    Work on node directories

---
+++ RobotRaconteurCore/include/RobotRaconteur/NodeDirectories.h
@@ -0,0 +1,151 @@
+/**
+ * @file NodeDirectories.h
+ *
+ * @author John Wason, PhD
+ *
+ * @copyright Copyright 2011-2022 Wason Technology, LLC
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * @par
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "RobotRaconteur/RobotRaconteurConstants.h"
+#include "RobotRaconteur/NodeID.h"
+
+#include <boost/filesystem/path.hpp>
+
+namespace RobotRaconteur
+{
+    /**
+     * @brief Directories on local system used by the node
+     * 
+     * The node uses local directories to load configuration information,
+     * cache data, communicate with other processes, etc. These directories
+     * can be configured using the NodeDirectories structure and
+     * RobotRaconteurNode::GetNodeDirectories() and RobotRaconteurNode::SetNodeDirectories()
+     * Use GetDefaultNodeDirectories() to retrieve the default directories.
+     * 
+     * Note: for root user, system and user directories are identical
+     */
+    class ROBOTRACONTEUR_CORE_API NodeDirectories
+    {
+    public:
+        /**
+         * @brief Robot Raconteur System data directory
+         * 
+         * Default value Unix : /usr/local/share/robotraconteur
+         * Default value Windows: %PROGRAMDATA%\RobotRaconteur\data
+         * Environmental variable override: ROBOTRACONTEUR_SYSTEM_DATA_DIR
+         */
+        boost::filesystem::path system_data_dir;
+        
+        /**
+         * @brief Robot Raconteur System config directory
+         * 
+         * Default value Unix: /etc/robotraconteur
+         * Default value Windows: %PROGRAMDATA%\RobotRaconteur\
+         * Environmental variable override: ROBOTRACONTEUR_SYSTEM_CONFIG_DIR
+         */
+        boost::filesystem::path system_config_dir;
+        
+        /**
+         * @brief Robot Raconteur System config directory
+         * 
+         * Default value Unix: /var/lib/robotraconteur
+         * Default value Windows: %PROGRAMDATA%\RobotRaconteur\state
+         * Environmental variable override: ROBOTRACONTEUR_SYSTEM_STATE_DIR
+         */
+        boost::filesystem::path system_state_dir;
+        
+        /**
+         * @brief Robot Raconteur System cache directory
+         * 
+         * Default value Unix: /var/cache/robotraconteur
+         * Default value Windows: %PROGRAMDATA%\RobotRaconteur\cache
+         * Environmental variable override: ROBOTRACONTEUR_SYSTEM_CACHE_DIR
+         */
+        boost::filesystem::path system_cache_dir;
+        
+        /**
+         * @brief Robot Raconteur System run directory
+         * 
+         * Default value: /var/run/robotraconteur
+         * Default value Windows: %PROGRAMDATA%\RobotRaconteur\run
+         * Environmental variable override: ROBOTRACONTEUR_SYSTEM_RUN_DIR
+         */
+        boost::filesystem::path system_run_dir;
+
+        /**
+         * @brief Robot Raconteur User data directory
+         * 
+         * Default value Unix: $HOME/.local/share/RobotRaconteur or $XDG_DATA_HOME/RobotRaconteur
+         * Default value Windows: %LOCALAPPDATA%\RobotRaconteur\data
+         * Environmental variable override: ROBOTRACONTEUR_USER_DATA_DIR
+         */
+        boost::filesystem::path user_data_dir;
+
+        /**
+         * @brief Robot Raconteur User config directory
+         * 
+         * Default value Unix: $HOME/.config/RobotRaconteur or $XDG_CONFIG_HOME/RobotRaconteur
+         * Default value Windows: %LOCALAPPDATA%\RobotRaconteur\
+         * Environmental variable override: ROBOTRACONTEUR_USER_CONFIG_DIR
+         */
+        boost::filesystem::path user_config_dir;
+
+        /**
+         * @brief Robot Raconteur User state directory
+         * 
+         * Default value Unix: $HOME/.local/state/RobotRaconteur or $XDG_STATE_HOME/RobotRaconteur
+         * Default value Windows: %LOCALAPPDATA%\RobotRaconteur\state
+         * Environmental variable override: ROBOTRACONTEUR_USER_STATE_DIR
+         */
+        boost::filesystem::path user_state_dir;
+
+        /**
+         * @brief Robot Raconteur User cache directory
+         * 
+         * Default value Unix: $HOME/.cache/RobotRaconteur or $XDG_CACHE_HOME/RobotRaconteur
+         * Default value Windows: %LOCALAPPDATA%\RobotRaconteur\cache
+         * Environmental variable override: ROBOTRACONTEUR_USER_CACHE_DIR
+         */
+        boost::filesystem::path user_cache_dir;
+
+        /**
+         * @brief Robot Raconteur User state directory
+         * 
+         * Default value Unix: $XDG_RUNTIME_DIR/robotraconteur or /tmp/robotraconteur-run-$UID
+         * Default value Windows: %LOCALAPPDATA%\RobotRaconteur\run
+         * Environmental variable override: ROBOTRACONTEUR_USER_RUN_DIR
+         */
+        boost::filesystem::path user_run_dir;
+    };
+
+    /**
+     * @brief Get the Default Node Directories object
+     * 
+     * @param node The node to send logging information (optional)
+     * @param create_user_dirs Create user directories if they don't exist (optional)
+     * @return NodeDirectories The default node directories
+     */
+    ROBOTRACONTEUR_CORE_API NodeDirectories GetDefaultNodeDirectories(RR_SHARED_PTR<RobotRaconteurNode> node = RR_SHARED_PTR<RobotRaconteurNode>(), bool create_user_dirs=true);
+
+    ROBOTRACONTEUR_CORE_API std::string GetLogonUserName();
+
+    ROBOTRACONTEUR_CORE_API bool IsLogonUserRoot();
+
+}
--- RobotRaconteurCore/src/NodeDirectories.cpp
+++ RobotRaconteurCore/src/NodeDirectories.cpp
@@ -1,10 +1,299 @@
 #include "RobotRaconteur/NodeDirectories.h"
+#include "RobotRaconteur/Logging.h"
+
+#ifdef ROBOTRACONTEUR_WINDOWS
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <Shlobj.h>
+#else
+#include <pwd.h>
+#endif
+
+#include <boost/filesystem.hpp>
+
 
 namespace RobotRaconteur
 {
 
+    namespace detail
+    {
+        static boost::filesystem::path replace_default_val_with_env(const boost::filesystem::path& default_val, const std::string& rr_env_var)
+        {
+            const char* env_val = std::getenv(rr_env_var.c_str());
+            if (env_val)
+            {
+                return boost::filesystem::path(env_val);
+                
+            }
+            return default_val;
+        }
+
+#ifndef ROBOTRACONTEUR_WINDOWS
+        static boost::filesystem::path user_unix_home_dir(const std::string& default_rel_dir, const std::string& xdg_env, const std::string& rr_env_var)
+        {
+            const char* rr_env_var_val = std::getenv(rr_env_var.c_str());
+            if (rr_env_var_val)
+            {
+                return boost::filesystem::path(rr_env_var_val);
+            }
+
+            const char* xdg_env_val = std::getenv(xdg_env.c_str());
+            if (xdg_env_val)
+            {
+                return boost::filesystem::path(xdg_env_val) / "RobotRaconteur";
+            }
+
+            const char* home = std::getenv("HOME");
+            if (!home)
+            {
+                throw SystemResourceException("Home directory not set");
+            }
+
+            return boost::filesystem::path(home) / default_rel_dir;
+        }
+
+        static boost::filesystem::path system_unix_dir(const std::string& default_dir, const std::string& rr_env_var)
+        {
+            const char* rr_env_var_val = std::getenv(rr_env_var.c_str());
+            if (rr_env_var_val)
+            {
+                return boost::filesystem::path(rr_env_var_val);
+            }
+
+            return boost::filesystem::path(default_dir);
+        }
+
+        static boost::filesystem::path user_unix_run_dir(const std::string& xdg_env, const std::string& rr_env_var)
+        {
+            const char* rr_env_var_val = std::getenv(rr_env_var.c_str());
+            if (rr_env_var_val)
+            {
+                return boost::filesystem::path(rr_env_var_val);
+            }
+
+            const char* xdg_env_val = std::getenv(xdg_env.c_str());
+            if (xdg_env_val)
+            {
+                return boost::filesystem::path(xdg_env_val) / "robotraconteur";
+            }
+
+            uid_t u = getuid();
+            std::string run_path = "/tmp/robotraconteur-run-" + boost::lexical_cast<std::string>(u);
+
+            return boost::filesystem::path(run_path);
+        }
+
+        static boost::filesystem::path user_apple_run_dir(const std::string& rr_env_var)
+        {
+            const char* rr_env_var_val = std::getenv(rr_env_var.c_str());
+            if (rr_env_var_val)
+            {
+                return boost::filesystem::path(rr_env_var_val);
+            }
+
+            char* path1 = std::getenv("TMPDIR");
+            if (!path1)
+                throw SystemResourceException("Could not activate system for local transport");
+
+            boost::filesystem::path path = path1;
+            path = path.remove_trailing_separator().parent_path();
+            path /= "C";
+            if (!boost::filesystem::is_directory(path))
+                throw SystemResourceException("Could not activate system for local transport");
+            path /= "robotraconteur";
+            return path;
+        }
+#endif
+
+#ifdef ROBOTRACONTEUR_WINDOWS
+        static boost::filesystem::path get_user_localappdata()
+        {
+            boost::scoped_array<char> sysdata_path1(new char[MAX_PATH]);
+            if (FAILED(SHGetFolderPath(NULL, CSIDL_LOCAL_APPDATA | CSIDL_FLAG_CREATE, NULL, 0, sysdata_path1.get())))
+            {
+                throw SystemResourceException("Could not get system information");
+            }
+
+            boost::filesystem::path sysdata_path(sysdata_path1.get());
+            return sysdata_path;
+        }
+
+        static boost::filesystem::path get_common_appdata()
+        {
+            boost::scoped_array<wchar_t> sysdata_path1(new wchar_t[MAX_PATH]);
+            if (FAILED(SHGetFolderPathW(NULL, CSIDL_COMMON_APPDATA | CSIDL_FLAG_CREATE, NULL, 0, sysdata_path1.get())))
+            {
+                throw SystemResourceException("Could not get system information");
+            }
+
+            boost::filesystem::path sysdata_path(sysdata_path1.get());
+            return sysdata_path;
+        }
+#endif
+
+    }
+
+    #define CATCH_DIR_RESOLVE(cmd, dir_type) \
+        try { \
+            cmd; \
+        } catch (std::exception& e) { \
+            if (node)   { \
+            ROBOTRACONTEUR_LOG_WARNING_COMPONENT(node, Node, -1, "Error resolving " << dir_type << ":" << e.what()); \
+        }}
+
     NodeDirectories GetDefaultNodeDirectories(RR_SHARED_PTR<RobotRaconteurNode> node, bool create_user_dirs)
     {
-        
+        NodeDirectories ret;
+#ifdef ROBOTRACONTEUR_WINDOWS
+        // Windows implementation
+        boost::filesystem::path user_localappdata = detail::get_user_localappdata();
+        boost::filesystem::path common_appdata = detail::get_common_appdata();
+
+        CATCH_DIR_RESOLVE(ret.system_data_dir = detail::replace_default_val_with_env(common_appdata / "RobotRaconteur" / "data", "ROBOTRACONTEUR_SYSTEM_DATA_DIR"),"system_data_dir");
+        CATCH_DIR_RESOLVE(ret.system_config_dir = detail::replace_default_val_with_env(common_appdata / "RobotRaconteur", "ROBOTRACONTEUR_SYSTEM_CONFIG_DIR"),"system_config_dir");
+        CATCH_DIR_RESOLVE(ret.system_state_dir = detail::replace_default_val_with_env(common_appdata / "RobotRaconteur" / "state", "ROBOTRACONTEUR_SYSTEM_STATE_DIR"), "system_state_dir");
+        CATCH_DIR_RESOLVE(ret.system_cache_dir = detail::replace_default_val_with_env(common_appdata / "RobotRaconteur" / "cache", "ROBOTRACONTEUR_SYSTEM_CACHE_DIR"),"system_cache_dir");
+        CATCH_DIR_RESOLVE(ret.system_run_dir = detail::replace_default_val_with_env(common_appdata / "RobotRaconteur" / "run", "ROBOTRACONTEUR_SYSTEM_RUN_DIR"), "system_run_dir");
+#else
+        CATCH_DIR_RESOLVE(ret.system_data_dir = detail::system_unix_dir("/usr/local/share/robotraconteur", "ROBOTRACONTEUR_SYSTEM_DATA_DIR"),"system_data_dir");
+        CATCH_DIR_RESOLVE(ret.system_config_dir = detail::system_unix_dir("/etc/robotraconteur", "ROBOTRACONTEUR_SYSTEM_CONFIG_DIR"),"system_config_dir");
+        CATCH_DIR_RESOLVE(ret.system_state_dir = detail::system_unix_dir("/var/lib/robotraconteur", "ROBOTRACONTEUR_SYSTEM_STATE_DIR"),"system_state_dir");
+        CATCH_DIR_RESOLVE(ret.system_cache_dir = detail::system_unix_dir("/var/cache/robotraconteur", "ROBOTRACONTEUR_SYSTEM_CACHE_DIR"),"system_cache_dir");
+        CATCH_DIR_RESOLVE(ret.system_run_dir = detail::system_unix_dir("/var/run/robotraconteur", "ROBOTRACONTEUR_SYSTEM_RUN_DIR"),"system_run_dir"); 
+#endif
+
+        if (IsLogonUserRoot())
+        {
+            ret.user_data_dir = ret.system_data_dir;
+            ret.user_config_dir = ret.system_config_dir;
+            ret.user_state_dir = ret.system_state_dir;
+            ret.user_cache_dir = ret.system_cache_dir;
+            ret.user_run_dir = ret.system_run_dir;
+        }
+        else
+        {
+#ifdef ROBOTRACONTEUR_WINDOWS
+
+            CATCH_DIR_RESOLVE(ret.user_data_dir = detail::replace_default_val_with_env(user_localappdata / "RobotRaconteur" / "data", "ROBOTRACONTEUR_USER_DATA_DIR"),"user_data_dir");
+            CATCH_DIR_RESOLVE(ret.user_config_dir = detail::replace_default_val_with_env(user_localappdata / "RobotRaconteur", "ROBOTRACONTEUR_USER_CONFIG_DIR"),"user_config_dir");
+            CATCH_DIR_RESOLVE(ret.user_state_dir = detail::replace_default_val_with_env(user_localappdata / "RobotRaconteur" / "state", "ROBOTRACONTEUR_USER_STATE_DIR"),"user_state_dir");
+            CATCH_DIR_RESOLVE(ret.user_cache_dir = detail::replace_default_val_with_env(user_localappdata / "RobotRaconteur" / "cache", "ROBOTRACONTEUR_USER_CACHE_DIR"),"user_cache_dir");
+            CATCH_DIR_RESOLVE(ret.user_run_dir = detail::replace_default_val_with_env(user_localappdata / "RobotRaconteur" / "run", "ROBOTRACONTEUR_USER_RUN_DIR"),"user_run_dir");
+
+#else
+            CATCH_DIR_RESOLVE(ret.user_data_dir = detail::user_unix_home_dir(".local/share/RobotRaconteur", "XDG_DATA_HOME", "ROBOTRACONTEUR_USER_DATA_DIR"),"user_data_dir");
+            CATCH_DIR_RESOLVE(ret.user_config_dir = detail::user_unix_home_dir(".config/RobotRaconteur", "XDG_CONFIG_HOME", "ROBOTRACONTEUR_USER_CONFIG_DIR"),"user_config_dir");
+            CATCH_DIR_RESOLVE(ret.user_state_dir = detail::user_unix_home_dir(".local/state/RobotRaconteur", "XDG_STATE_HOME", "ROBOTRACONTEUR_USER_CONFIG_DIR"),"user_state_dir");
+            CATCH_DIR_RESOLVE(ret.user_cache_dir = detail::user_unix_home_dir(".cache/RobotRaconteur", "XDG_CACHE_HOME", "ROBOTRACONTEUR_USER_CACHE_DIR"),"user_cache_dir");
+
+#ifdef ROBOTRACONTEUR_APPLE
+            CATCH_DIR_RESOLVE(ret.user_run_dir = detail::user_apple_run_dir("ROBOTRACONTEUR_USER_RUN_DIR"),"user_run_dir)";
+#else
+            CATCH_DIR_RESOLVE(ret.user_run_dir = detail::user_unix_run_dir("XDG_RUNTIME_DIR", "ROBOTRACONTEUR_USER_RUN_DIR"),"user_run_dir");
+#endif
+
+#endif
+        }
+
+        return ret;
     }
+
+    std::string GetLogonUserName()
+    {
+    #ifdef ROBOTRACONTEUR_WINDOWS
+        DWORD size = 0;
+        GetUserName(NULL, &size);
+
+        boost::scoped_array<char> n(new char[size]);
+
+        BOOL res = GetUserName(n.get(), &size);
+        if (!res)
+            throw SystemResourceException("Could not determine current user");
+
+        return std::string(n.get());
+    #else
+        struct passwd* passwd = NULL;
+        passwd = getpwuid(getuid());
+        if (passwd == NULL)
+            throw ConnectionException("Could not determine current user");
+
+        std::string o(passwd->pw_name);
+        if (o.empty())
+            throw ConnectionException("Could not determine current user");
+
+        return o;
+
+    #endif
+    }
+
+
+    #ifdef ROBOTRACONTEUR_WINDOWS
+
+    bool IsLogonUserRoot_win_compare_rid(PTOKEN_USER pTokenUser, DWORD rid)
+    {
+        SID_IDENTIFIER_AUTHORITY siaNT = SECURITY_NT_AUTHORITY;
+        PSID pSystemSid;
+        if (!AllocateAndInitializeSid(&siaNT, 1, rid,
+            0, 0, 0, 0, 0, 0, 0, &pSystemSid))
+            return FALSE;
+
+        // compare the user SID from the token with the LocalSystem SID
+        BOOL res = EqualSid(pTokenUser->User.Sid, pSystemSid);
+        FreeSid(pSystemSid);
+
+        return res != 0;
+    }
+
+    bool IsLogonUserRoot_win()
+    {
+        HANDLE hToken;
+        UCHAR bTokenUser[sizeof(TOKEN_USER) + 8 + 4 * SID_MAX_SUB_AUTHORITIES];
+        PTOKEN_USER pTokenUser = (PTOKEN_USER)bTokenUser;
+        ULONG cbTokenUser;
+
+        // open process token
+        if (!OpenProcessToken(GetCurrentProcess(),
+            TOKEN_QUERY,
+            &hToken))
+            return false;
+
+        // retrieve user SID
+        if (!GetTokenInformation(hToken, TokenUser, pTokenUser,
+            sizeof(bTokenUser), &cbTokenUser))
+        {
+            CloseHandle(hToken);
+            return false;
+        }
+
+        CloseHandle(hToken);
+
+        if (IsLogonUserRoot_win_compare_rid(pTokenUser, SECURITY_LOCAL_SYSTEM_RID))
+        {
+            return true;
+        }
+
+        if (IsLogonUserRoot_win_compare_rid(pTokenUser, SECURITY_LOCAL_SERVICE_RID))
+        {
+            return true;
+        }
+
+        if (IsLogonUserRoot_win_compare_rid(pTokenUser, SECURITY_NETWORK_SERVICE_RID))
+        {
+            return true;
+        }
+        return false;
+    }
+    #endif
+
+    bool IsLogonUserRoot()
+    {
+    #ifndef ROBOTRACONTEUR_WINDOWS
+        uid_t user_uid = getuid();
+        return user_uid == 0;
+    #else
+        return IsLogonUserRoot_win();
+    #endif
+
+    }
+
 }
