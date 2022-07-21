884f6ad657ef1bae85adb8d0a438b2146eabf1bb
John Wason <wason@wasontech.com>
2022-07-20 21:50:51 -0400

    Create header file for boost asio windows socket backport

--- RobotRaconteurCore/src/Tap.cpp
+++ RobotRaconteurCore/src/Tap.cpp
@@ -12,43 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#define BOOST_ASIO_HAS_LOCAL_SOCKETS 1
-
-#ifdef _WIN32
-#include <WinSock2.h>
-#include <versionhelpers.h>
-
-#ifdef IO_REPARSE_TAG_AF_UNIX
-#include <afunix.h>
-#else
-// Need to copy over sockaddr_un due to missing afunix.h
-#define UNIX_PATH_MAX 108
-
-typedef struct sockaddr_un
-{
-    ADDRESS_FAMILY sun_family;    /* AF_UNIX */
-    char sun_path[UNIX_PATH_MAX]; /* pathname */
-} SOCKADDR_UN, *PSOCKADDR_UN;
-#endif
-
-#include <boost/asio/version.hpp>
-
-#if BOOST_ASIO_VERSION < 101801
-namespace boost
-{
-namespace asio
-{
-namespace detail
-{
-typedef sockaddr_un sockaddr_un_type;
-}
-} // namespace asio
-} // namespace boost
-
-#include <boost/asio/local/stream_protocol.hpp>
-#endif
-
-#endif
+#include "boost_asio_win_unix_sockets_backport.h"
 
 #include <boost/bind/placeholders.hpp>
 #include <boost/asio.hpp>
