16c8f1c04e5daae712e8b29c52ab59f8c09b0675
John Wason <wason@wasontech.com>
2023-06-30 11:21:00 +0000

    Code formatting

--- packaging/ios/ports/robotraconteur/portfile.cmake
+++ packaging/ios/ports/robotraconteur/portfile.cmake
@@ -1,14 +1,14 @@
 include(vcpkg_common_functions)
 
 vcpkg_configure_cmake(
-    SOURCE_PATH ${CMAKE_CURRENT_LIST_DIR}/../../..
-	OPTIONS
-	    -DBUILD_GEN=OFF
-		-DBUILD_TEST=OFF
-		-DBUILD_TESTING=OFF
-		-DBUILD_SHARED_LIBS=OFF
-		-DOPENSSL_USE_STATIC_LIBS=ON
-)
+    SOURCE_PATH
+    ${CMAKE_CURRENT_LIST_DIR}/../../..
+    OPTIONS
+    -DBUILD_GEN=OFF
+    -DBUILD_TEST=OFF
+    -DBUILD_TESTING=OFF
+    -DBUILD_SHARED_LIBS=OFF
+    -DOPENSSL_USE_STATIC_LIBS=ON)
 
 vcpkg_install_cmake()
 
