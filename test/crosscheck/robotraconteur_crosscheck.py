fd66fdb0112d12adf5fbcc9cce3c4a7d14624cb5
John Wason <wason@wasontech.com>
2023-09-19 22:10:26 +0000

    Code formatting

--- test/crosscheck/robotraconteur_crosscheck.py
+++ test/crosscheck/robotraconteur_crosscheck.py
@@ -17,6 +17,7 @@ object Crosscheck
 end
 """
 
+
 class crosscheck_impl:
     def __init__(self, node):
         self.node = node
@@ -24,15 +25,14 @@ class crosscheck_impl:
     @property
     def version_robotraconteur(self):
         return self.node.RobotRaconteurVersion
-    
+
     @property
     def hostname(self):
         return socket.gethostname()
-    
+
     @property
     def platform(self):
         return sys.platform
-
 
 
 class crosscheck_server_nodes:
@@ -50,26 +50,29 @@ class crosscheck_server_nodes:
         self.insecure_node.Init()
         self.insecure_obj = crosscheck_impl(self.insecure_node)
         self.insecure_node.RegisterServiceType(_crosscheck_robdef)
-        self.insecure_node.RegisterService("crosscheck", "experimental.crosscheck.Crosscheck", self.insecure_obj)
-        self.insecure_node_setup = RR.ServerNodeSetup(args.insecure_nodename, args.insecure_tcp_port,  
+        self.insecure_node.RegisterService(
+            "crosscheck", "experimental.crosscheck.Crosscheck", self.insecure_obj)
+        self.insecure_node_setup = RR.ServerNodeSetup(args.insecure_nodename, args.insecure_tcp_port,
                                                       node=self.insecure_node)
-        
+
         if args.secure_enabled:
             self.secure_node = RR.RobotRaconteurNode()
             self.secure_node.Init()
             self.secure_obj = crosscheck_impl(self.secure_node)
             self.secure_node.RegisterServiceType(_crosscheck_robdef)
-            self.secure_node.RegisterService("crosscheck", "experimental.crosscheck.Crosscheck", self.secure_obj)
+            self.secure_node.RegisterService(
+                "crosscheck", "experimental.crosscheck.Crosscheck", self.secure_obj)
             self.secure_node_setup = RR.SecureServerNodeSetup(args.secure_nodename, args.secure_tcp_port,
-                                                        node=self.secure_node)
-        
+                                                              node=self.secure_node)
+
     def close(self):
         if self.insecure_node_setup:
             self.insecure_node_setup.close()
 
         if self.secure_node_setup:
             self.secure_node_setup.close()
-        
+
+
 class crosscheck_client:
     def __init__(self, json=False):
 
@@ -78,20 +81,21 @@ class crosscheck_client:
             | RR.RobotRaconteurNodeSetupFlags_ENABLE_NODE_DISCOVERY_LISTENING \
             | RR.RobotRaconteurNodeSetupFlags_DISABLE_STRINGTABLE
 
-        
-
         self.json = json
         self.node = RR.RobotRaconteurNode()
         self.node.Init()
-        self.node_setup = RR.RobotRaconteurNodeSetup("", 0, client_node_flags, node=self.node)
+        self.node_setup = RR.RobotRaconteurNodeSetup(
+            "", 0, client_node_flags, node=self.node)
 
         insecure_sub_filter = RR.ServiceSubscriptionFilter()
         insecure_sub_filter.TransportSchemes = ["rr+tcp"]
-        self.insecure_sub = self.node.SubscribeServiceByType("experimental.crosscheck.Crosscheck", insecure_sub_filter)
+        self.insecure_sub = self.node.SubscribeServiceByType(
+            "experimental.crosscheck.Crosscheck", insecure_sub_filter)
 
         secure_sub_filter = RR.ServiceSubscriptionFilter()
         secure_sub_filter.TransportSchemes = ["rrs+tcp"]
-        self.secure_sub = self.node.SubscribeServiceByType("experimental.crosscheck.Crosscheck", secure_sub_filter)
+        self.secure_sub = self.node.SubscribeServiceByType(
+            "experimental.crosscheck.Crosscheck", secure_sub_filter)
 
         self.timer = self.node.CreateTimer(1, self.timer_cb)
         self.timer.Start()
@@ -144,21 +148,29 @@ class crosscheck_client:
             }))
             sys.stdout.flush()
 
-
     def close(self):
         if self.node_setup:
             self.node_setup.close()
 
+
 def main():
-    
-    parser = argparse.ArgumentParser(description="tool to check Robot Raconteur cross-platform compatibility over the network")
-    parser.add_argument("--servers-enabled", type=bool, default=True, help="Enable server nodes")
-    parser.add_argument("--insecure-nodename", type=str, default="crosscheck", help="Node name for insecure server node")
-    parser.add_argument("--insecure-tcp-port", type=int, default=0, help="TCP listen port for insecure server node")
-    parser.add_argument("--secure-enabled", type=bool, default=True, help="Enable secure server node")
-    parser.add_argument("--secure-nodename", type=str, default="crosscheck_secure", help="Node name for secure server node")
-    parser.add_argument("--secure-tcp-port", type=int, default=0, help="TCP lister port for secure server node")
-    parser.add_argument("--json", action="store_true", help="Output in JSON format")
+
+    parser = argparse.ArgumentParser(
+        description="tool to check Robot Raconteur cross-platform compatibility over the network")
+    parser.add_argument("--servers-enabled", type=bool,
+                        default=True, help="Enable server nodes")
+    parser.add_argument("--insecure-nodename", type=str,
+                        default="crosscheck", help="Node name for insecure server node")
+    parser.add_argument("--insecure-tcp-port", type=int, default=0,
+                        help="TCP listen port for insecure server node")
+    parser.add_argument("--secure-enabled", type=bool,
+                        default=True, help="Enable secure server node")
+    parser.add_argument("--secure-nodename", type=str,
+                        default="crosscheck_secure", help="Node name for secure server node")
+    parser.add_argument("--secure-tcp-port", type=int, default=0,
+                        help="TCP lister port for secure server node")
+    parser.add_argument("--json", action="store_true",
+                        help="Output in JSON format")
 
     args = parser.parse_args()
 
