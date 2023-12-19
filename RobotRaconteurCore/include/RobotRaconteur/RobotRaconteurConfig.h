725dff547051f490b826f4f62703e4fdd56457cb
John Wason <wason@wasontech.com>
2023-12-19 04:39:15 -0500

    Work on tls over websocket

--- RobotRaconteurCore/include/RobotRaconteur/RobotRaconteurConfig.h
+++ RobotRaconteurCore/include/RobotRaconteur/RobotRaconteurConfig.h
@@ -110,9 +110,11 @@
 #if !defined(BOOST_NO_CXX11_RVALUE_REFERENCES)
 #define RR_MOVE_ARG(type) type&&
 #define RR_MOVE(x) std::move(x)
+#define RR_FORWARD(type, x) std::forward<type>(x)
 #else
 #define RR_MOVE_ARG(type) type
 #define RR_MOVE(x) x
+#define RR_FORWARD(type, x) x
 #endif
 
 #include <boost/asio/version.hpp>
@@ -120,18 +122,26 @@
 #if BOOST_ASIO_VERSION < 101200
 #define RR_BOOST_ASIO_IO_CONTEXT boost::asio::io_service
 #define RR_BOOST_ASIO_STRAND boost::asio::io_service::strand
+#define RR_BOOST_ASIO_STRAND2(exec_type) boost::asio::io_service::strand
 #define RR_BOOST_ASIO_POST(context, func) context.post(func)
 #define RR_BOOST_ASIO_BUFFER_CAST(type, buf) boost::asio::buffer_cast<type>(buf)
 #define RR_BOOST_ASIO_STRAND_WRAP(strand, f) (strand).wrap(f)
 #define RR_BOOST_ASIO_NEW_STRAND(context) (new boost::asio::strand(context))
+#define RR_BOOST_ASIO_GET_IO_SERVICE(s) (s).get_io_service()
+#define RR_BOOST_ASIO_MAKE_STRAND(x) boost::asio::strand(x)
+#define RR_BOOST_ASIO_REF_IO_SERVICE(x) boost::ref(x)
 #else
 #define RR_BOOST_ASIO_IO_CONTEXT boost::asio::io_context
 #define RR_BOOST_ASIO_STRAND boost::asio::strand<boost::asio::io_context::executor_type>
+#define RR_BOOST_ASIO_STRAND2(exec_type) boost::asio::strand<exec_type>
 #define RR_BOOST_ASIO_POST(context, func) boost::asio::post(context, func)
 #define RR_BOOST_ASIO_BUFFER_CAST(type, buf) (type)(buf).data()
 #define RR_BOOST_ASIO_STRAND_WRAP(strand, f) boost::asio::bind_executor(strand, f)
 #define RR_BOOST_ASIO_NEW_STRAND(context)                                                                              \
     (new boost::asio::strand<boost::asio::io_context::executor_type>((context).get_executor()))
+#define RR_BOOST_ASIO_MAKE_STRAND(x) boost::asio::make_strand(x)
+#define RR_BOOST_ASIO_GET_IO_SERVICE(s) (s).get_executor()
+#define RR_BOOST_ASIO_REF_IO_SERVICE(x) (x)
 #endif
 
 #if BOOST_VERSION <= 105900
--- RobotRaconteurCore/src/asio_ssl_stream_threadsafe.h
+++ RobotRaconteurCore/src/asio_ssl_stream_threadsafe.h
@@ -12,47 +12,82 @@ namespace detail
 
         typedef typename boost::remove_reference<boost::asio::ssl::stream<Stream> >::type next_layer_type;
         typedef typename next_layer_type::lowest_layer_type lowest_layer_type;
-        typedef typename lowest_layer_type::executor_type executor_type;
         typedef typename next_layer_type::native_handle_type native_handle_type;
-    
+
+#if BOOST_ASIO_VERSION >= 101200
+        typedef typename lowest_layer_type::executor_type executor_type;
+#else
+        typedef boost::asio::io_service executor_type;
+#endif
     protected:    
         boost::asio::ssl::stream<Stream> ssl_stream_;
         boost::asio::ssl::context& context_;
-        boost::asio::strand<executor_type> strand_;
+        RR_BOOST_ASIO_STRAND2(executor_type) strand_;
+
+        template <typename Handler, typename Executor>
+        class handler_wrapper
+        {
+        public:
+            typedef typename boost::remove_reference<Handler>::type HandlerValueType;
+
+            handler_wrapper(const Handler& handler, Executor& executor) : handler_(RR_MOVE(handler)),
+                executor_(executor) {}
+
+            void operator()(const boost::system::error_code& ec, const std::size_t& bytes_transferred)
+            {
+                boost::asio::detail::binder2<HandlerValueType, boost::system::error_code, std::size_t>
+                handler(handler_, ec, bytes_transferred);
+#if BOOST_ASIO_VERSION >= 101200
+                boost::asio::post(boost::asio::get_associated_executor(handler, executor_),handler);
+#else
+                executor_.post(handler);
+#endif
+            }
+
+        private:
+            HandlerValueType handler_;
+#if BOOST_ASIO_VERSION >= 101200
+            Executor executor_;
+#else
+            boost::asio::io_service& executor_;
+#endif
+        };
 
     public:
 
         asio_ssl_stream_threadsafe(Stream& stream, boost::asio::ssl::context& context)
-            : ssl_stream_(stream, context), context_(context), strand_(boost::asio::make_strand(stream.get_executor()))
+            : ssl_stream_(stream, context), context_(context), strand_(RR_BOOST_ASIO_MAKE_STRAND(RR_BOOST_ASIO_GET_IO_SERVICE(stream)))
         {
         }
 
         // async_read_some using strand for callback
         template <typename MutableBufferSequence, typename ReadHandler>
-        void async_read_some(const MutableBufferSequence& buffers, ReadHandler&& handler)
+        void async_read_some(const MutableBufferSequence& buffers, RR_MOVE_ARG(ReadHandler) handler)
         {
-            ssl_stream_.async_read_some(buffers, boost::asio::bind_executor(strand_,std::forward<ReadHandler>(handler)));
+            handler_wrapper<ReadHandler, executor_type> handler2(boost::ref(handler), RR_BOOST_ASIO_GET_IO_SERVICE((*this)));
+            ssl_stream_.async_read_some(buffers, RR_BOOST_ASIO_STRAND_WRAP(strand_,RR_MOVE(handler2)));
         }
 
         // async_write_some using strand for callback
         template <typename ConstBufferSequence, typename WriteHandler>
-        void async_write_some(const ConstBufferSequence& buffers, WriteHandler&& handler)
+        void async_write_some(const ConstBufferSequence& buffers, RR_MOVE_ARG(WriteHandler) handler)
         {
-            ssl_stream_.async_write_some(buffers, boost::asio::bind_executor(strand_, std::forward<WriteHandler>(handler)));
+            handler_wrapper<WriteHandler, executor_type> handler2(boost::ref(handler), RR_BOOST_ASIO_GET_IO_SERVICE((*this)));
+            ssl_stream_.async_write_some(buffers, RR_BOOST_ASIO_STRAND_WRAP(strand_, RR_MOVE(handler2)));
         }
 
         // async_handshake using strand for callback
         template <typename HandshakeHandler>
-        void async_handshake(boost::asio::ssl::stream_base::handshake_type type, HandshakeHandler&& handler)
+        void async_handshake(boost::asio::ssl::stream_base::handshake_type type, RR_MOVE_ARG(HandshakeHandler) handler)
         {
-            ssl_stream_.async_handshake(type, boost::asio::bind_executor(strand_, std::forward<HandshakeHandler>(handler)));
+            ssl_stream_.async_handshake(type, RR_BOOST_ASIO_STRAND_WRAP(strand_, RR_FORWARD(HandshakeHandler, handler)));
         }
 
         // async_shutdown using strand for callback
         template <typename ShutdownHandler>
-        void async_shutdown(ShutdownHandler&& handler)
+        void async_shutdown(RR_MOVE_ARG(ShutdownHandler) handler)
         {
-            ssl_stream_.async_shutdown(boost::asio::bind_executor(strand_, std::forward<ShutdownHandler>(handler)));
+            ssl_stream_.async_shutdown(RR_BOOST_ASIO_STRAND_WRAP(strand_, RR_FORWARD(ShutdownHandler, handler)));
         }
 
         native_handle_type native_handle()
@@ -75,10 +110,14 @@ namespace detail
             ssl_stream_.set_verify_mode(v);
         }
 
-        executor_type get_executor() noexcept
+#if BOOST_ASIO_VERSION >= 101200
+        executor_type get_executor() BOOST_ASIO_NOEXCEPT
         {
             return ssl_stream_.lowest_layer().get_executor();
-        }           
+        }
+#else
+        boost::asio::io_service& get_io_service() { return ssl_stream_.lowest_layer().get_io_service(); }
+#endif
 
     };
 }
