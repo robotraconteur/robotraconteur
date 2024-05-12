5bcbef5fd6c0e6c314b1b0dbadf40e9f728a9015
John Wason <wason@wasontech.com>
2024-05-11 22:56:08 -0400

    c++ examples work

--- examples/reynard_the_robot/cpp/service/reynard_robotraconteur_service_ascii_socket.cpp
+++ examples/reynard_the_robot/cpp/service/reynard_robotraconteur_service_ascii_socket.cpp
@@ -1,4 +1,5 @@
-// C++ example Robot Raconteur service for Reynard the Robot using the HTTP REST API to communicate with the robot.
+// C++ example Robot Raconteur service for Reynard the Robot using the ASCII Socket intreface to communicate 
+// with the robot.
 
 // Note that Reynard the Robot uses mm and degrees, while Robot Raconteur services are expected
 // to use meters and radians. The service will convert the units as needed.
@@ -8,14 +9,9 @@
 #include <RobotRaconteur.h>
 #include "robotraconteur_generated.h"
 
-#include <boost/json/src.hpp>
-#include "json_body.hpp"
-
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/beast/version.hpp>
-
 #include "drekar_launch_process_cpp/drekar_launch_process_cpp.h"
+
+#include <boost/asio/ip/tcp.hpp>
 
 // Only use the RR alias in cpp files. Do not use it in header files.
 
@@ -32,114 +28,102 @@ namespace RR = RobotRaconteur;
 // Underscores are used in the class name to avoid conflicts with the generated class name. This is not required,
 // but is a common practice.
 
+// This example uses a simple ASCII socket interface to communicate with the robot. Many industrial devices
+// will use an ASCII socket interface or serial port interface. Many other devices will use a binary interface.
+// Binary interfaces are more efficient but are also more complicated. See the iRobot Create example for a
+// binary serial port interface example. Many devices that use a binary socket interface will provide a client
+// library that implements the protocol.
+
 class Reynard_impl : public virtual experimental::reynard_the_robot::Reynard_default_impl,
     public RR_ENABLE_SHARED_FROM_THIS<Reynard_impl>
 {
 protected:
 
-    std::string hostname;
-    int32_t port;
+    boost::asio::ip::tcp::endpoint reynard_socket_endpoint;
     RR::TimerPtr state_timer;
 
 public:
-    Reynard_impl(const std::string& hostname, int32_t port)
+    Reynard_impl(const boost::asio::ip::tcp::endpoint& reynard_socket_endpoint)
     {
-        this->hostname = hostname;
-        this->port = port;
+        this->reynard_socket_endpoint = reynard_socket_endpoint;
     }
 
-    boost::json::value _get_json(const std::string& path)
+    // _communicate is a simple request and response function that opens a socket, sends a request, and reads a response.
+    // More sophisticated devices may keep the socket open and reuse it for multiple requests. This is more efficient,
+    // but requires more complicated logic. This function is also synchronous, which is less efficient
+    // but simpler for this example.
+    std::vector<std::string> _communicate(const std::string& text_request, const std::string& expected_response_op)
     {
-        // Based on https://www.boost.org/doc/libs/1_85_0/libs/beast/example/http/client/body/json_client.cpp
-        boost::asio::io_context ioc;
-        boost::asio::ip::tcp::resolver resolver(ioc);
-        boost::beast::tcp_stream stream(ioc);
+        // Create and connect a socket stream
+        boost::asio::ip::tcp::iostream s;
+        s.connect(reynard_socket_endpoint);
 
-        auto const results = resolver.resolve(hostname, std::to_string(port));
-        stream.connect(results);
-
-        boost::beast::http::request<boost::beast::http::empty_body> req{boost::beast::http::verb::get, path, 11};
-        req.set(boost::beast::http::field::host, hostname);
-        req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-
-        boost::beast::http::write(stream, req);
-
-        boost::beast::flat_buffer buffer;
-        boost::beast::http::response<json_body> res;
-        boost::beast::http::read(stream, buffer, res);
-
-        boost::json::value ret = res.body();
-
-        stream.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both);
-
-        return ret;
-    }
-
-    boost::json::value _post_json(const std::string& path, const boost::json::value& obj)
-    {
-        // Based on https://www.boost.org/doc/libs/1_85_0/libs/beast/example/http/client/body/json_client.cpp
-        boost::asio::io_context ioc;
-        boost::asio::ip::tcp::resolver resolver(ioc);
-        boost::beast::tcp_stream stream(ioc);
-
-        auto const results = resolver.resolve(hostname, std::to_string(port));
-        stream.connect(results);
-
-        // Set up an HTTP POST request message
-        boost::beast::http::request<json_body> req{boost::beast::http::verb::post, path, 11};
-        req.set(boost::beast::http::field::host, hostname);
-        req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-        req.set(boost::beast::http::field::content_type, "application/json");
-        req.body() = obj;
-        req.prepare_payload();
-        // Send the HTTP request to the remote host
-        boost::beast::http::write(stream, req);
-
-        boost::beast::flat_buffer buffer;
-        boost::beast::http::response<json_body> res;
-
-        boost::json::value ret;
-        try
+        if (!s)
         {
-            boost::beast::http::read(stream, buffer, res);
-            ret = res.body();
+            throw RR::ConnectionException("Could not connect to Reynard the Robot");
         }
-        catch (const boost::system::system_error& e)
+
+        // Send the request text and newline
+        s << text_request << "\n";
+
+        // Read the response line
+        std::string response;
+        std::getline(s, response);
+        boost::trim(response);
+
+        // Check the response
+        if (boost::starts_with(response, "ERROR"))
         {
-            if (e.code() != boost::json::error::incomplete)
+            throw RR::OperationFailedException("Error from robot: " + response);
+        }
+
+        // OK has no return arguments
+        if (expected_response_op == "OK")
+        {
+            if (response != "OK")
             {
-                // incomplete_json is expected if the response is empty
-                throw;
+                throw RR::OperationFailedException("Unexpected response from robot: " + response);
             }
+            return std::vector<std::string>();
         }
 
-        stream.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+        // Split the response into arguments
+        std::vector<std::string> response_args;
+        boost::split(response_args, response, boost::is_any_of(" "));
+        if (response_args.size() == 0)
+        {
+            throw RR::OperationFailedException("Invalid response from robot: " + response);
+        }
 
+        // Check the operation name
+        if (response_args[0] != expected_response_op)
+        {
+            throw RR::OperationFailedException("Unexpected response from robot: " + response);
+        }
+
+        std::vector<std::string> ret(response_args.begin() + 1, response_args.end());
+        
+        s.close();
         return ret;
     }
 
     RR::RRArrayPtr<double > get_robot_position() override
     {
-        auto json_ret = _get_json("/api/state");
-        double x = json_ret.at("x").as_double();
-        double y = json_ret.at("y").as_double();
+        auto socket_res = _communicate("STATE", "STATE");
 
         auto ret = RR::AllocateRRArray<double>(2);
-        ret->at(0) = x * 1.0e-3; // Convert to meters 
-        ret->at(1) = y * 1.0e-3; // Convert to meters
+        ret->at(0) = boost::lexical_cast<double>(socket_res[0]) * 1.0e-3; // Convert to meters
+        ret->at(1) = boost::lexical_cast<double>(socket_res[1]) * 1.0e-3; // Convert to meters
         return ret;
     }
     RR::RRArrayPtr<double > get_color() override
     {
-        auto json_ret = _get_json("/api/color");
-        double r = json_ret.at("r").as_double();
-        double g = json_ret.at("g").as_double();
-        double b = json_ret.at("b").as_double();
+        auto socket_res = _communicate("COLORGET", "COLORGET");
 
         auto ret = RR::AllocateRRArray<double>(3);
-        ret->at(0) = r;
-        ret->at(1) = g;
-        ret->at(2) = b;
+        ret->at(0) = boost::lexical_cast<double>(socket_res[0]);
+        ret->at(1) = boost::lexical_cast<double>(socket_res[1]);
+        ret->at(2) = boost::lexical_cast<double>(socket_res[2]);
         return ret;
     }
     void set_color(const RR::RRArrayPtr<double >& value) override
@@ -149,27 +133,18 @@ public:
             throw RR::InvalidArgumentException("Color must be a 3 element array");
         }
 
-        double r = value->at(0);
-        double g = value->at(1);
-        double b = value->at(2);
-
-        boost::json::value obj = boost::json::object{
-            {"r", r},
-            {"g", g},
-            {"b", b}
-        };
+        std::stringstream ss;
+        ss << "COLORSET " << value->at(0) << " " << value->at(1) << " " << value->at(2);
+        _communicate(ss.str(), "OK");
     }
 
     void teleport(double x, double y) override
     {
         x = x * 1.0e3; // Convert to mm
         y = y * 1.0e3; // Convert to mm
-        boost::json::value obj = boost::json::object{
-            {"x", x},
-            {"y", y}
-        };
-
-        _post_json("/api/teleport", obj);
+        std::stringstream ss;
+        ss << "TELEPORT " << x << " " << y;
+        _communicate(ss.str(), "OK");
     }
 
     void setf_arm_position(double q1, double q2, double q3) override
@@ -177,21 +152,19 @@ public:
         q1 = q1 * (180.0 / M_PI); // Convert to degrees
         q2 = q2 * (180.0 / M_PI); // Convert to degrees
         q3 = q3 * (180.0 / M_PI); // Convert to degrees
-        boost::json::value obj = boost::json::object{
-            {"q1", q1},
-            {"q2", q2},
-            {"q3", q3}
-        };
-
-        _post_json("/api/arm", obj);
+        std::stringstream ss;
+        ss << "SETARM " << q1 << " " << q2 << " " << q3;
+        _communicate(ss.str(), "OK");
     }
 
     RR::RRArrayPtr<double > getf_arm_position() override
     {
-        auto json_ret = _get_json("/api/state");
-        double q1 = json_ret.at("q1").as_double();
-        double q2 = json_ret.at("q2").as_double();
-        double q3 = json_ret.at("q3").as_double();
+        
+        auto socket_res = _communicate("STATE", "STATE");
+
+        double q1 = boost::lexical_cast<double>(socket_res[2]);
+        double q2 = boost::lexical_cast<double>(socket_res[3]);
+        double q3 = boost::lexical_cast<double>(socket_res[4]);
 
         auto ret = RR::AllocateRRArray<double>(3);
         ret->at(0) = q1 * (M_PI / 180.0); // Convert to radians
@@ -209,12 +182,10 @@ public:
 
         vel_x = vel_x * 1.0e3; // Convert to mm/s
         vel_y = vel_y * 1.0e3; // Convert to mm/s
-        boost::json::value obj = boost::json::object{
-            {"vel_x", vel_x},
-            {"vel_y", vel_y}
-        };
-
-        _post_json("/api/drive_robot", obj);
+        
+        std::stringstream ss;
+        ss << "DRIVE " << vel_x << " " << vel_y;
+        _communicate(ss.str(), "OK");
     }
 
     void drive_arm(double q1, double q2, double q3, double timeout, RobotRaconteur::rr_bool wait) override
@@ -228,41 +199,33 @@ public:
         q2 = q2 * (180.0 / M_PI); // Convert to degrees
         q3 = q3 * (180.0 / M_PI); // Convert to degrees
 
-        boost::json::value obj = boost::json::object{
-            {"q1", q1},
-            {"q2", q2},
-            {"q3", q3}
-        };
-
-        _post_json("/api/drive_arm", obj);
+        std::stringstream ss;
+        ss << "DRIVEARM " << q1 << " " << q2 << " " << q3;
+        _communicate(ss.str(), "OK");
     }
 
     void say(const std::string& message) override
     {
-        boost::json::value obj = boost::json::object{
-            {"message", message}
-        };
-
-        _post_json("/api/say", obj);
+        std::stringstream ss;
+        ss << "SAY \"" << message << "\"";
+        _communicate(ss.str(), "OK");
     }
 
 
     void _timer_cb(const RR::TimerEvent& ev)
     {   
-
-        // Many HTTP devices (such as ABB robot controllers) provide a WebSocket to provide asynchronous updates.
-        // This is not available in the Reynard the Robot API, so we must poll the state.
         try
         {
-            auto json_ret = _get_json("/api/state");
-            // JSON response only contains the robot position and arm position
-            double x = json_ret.at("x").as_double();
-            double y = json_ret.at("y").as_double();
-            double q1 = json_ret.at("q1").as_double();
-            double q2 = json_ret.at("q2").as_double();
-            double q3 = json_ret.at("q3").as_double();
+            auto socket_res = _communicate("STATE", "STATE");
+            double t = boost::lexical_cast<double>(socket_res.at(0));
+            double x = boost::lexical_cast<double>(socket_res.at(1));
+            double y = boost::lexical_cast<double>(socket_res.at(2));
+            double q1 = boost::lexical_cast<double>(socket_res.at(3));
+            double q2 = boost::lexical_cast<double>(socket_res.at(4));
+            double q3 = boost::lexical_cast<double>(socket_res.at(5));
             
             experimental::reynard_the_robot::ReynardStatePtr state(new experimental::reynard_the_robot::ReynardState());
+            state->time = t;
             state->robot_position = RR::AllocateRRArray<double>(2);
             state->robot_position->at(0) = x * 1.0e-3; // Convert to meters
             state->robot_position->at(1) = y * 1.0e-3; // Convert to meters
@@ -270,18 +233,11 @@ public:
             state->arm_position->at(0) = q1 * (M_PI / 180.0); // Convert to radians
             state->arm_position->at(1) = q2 * (M_PI / 180.0); // Convert to radians
             state->arm_position->at(2) = q3 * (M_PI / 180.0); // Convert to radians
-            // Velocity is not available in the JSON API
+            // Velocity is not available in the ASCII interface
 
             if (rrvar_state)
             {
                 rrvar_state->SetOutValue(state);
-            }
-
-            auto message_json_ret = _get_json("/api/messages");
-            for (auto& message : message_json_ret.as_array())
-            {
-                std::string message_str = message.at("message").as_string().c_str();
-                rrvar_new_message(message_str);
             }
         }
         catch (std::exception& exp)
@@ -317,11 +273,27 @@ public:
 
 int main(int argc, char* argv[])
 {
+
+    // Resolve the socket endpoint
+    std::string reynard_host = "localhost";
+    int reynard_port = 29202;
+    boost::asio::io_context resolve_io_context;
+    boost::asio::ip::tcp::resolver resolver(resolve_io_context);
+    boost::asio::ip::tcp::resolver::query resolver_query(boost::asio::ip::tcp::v4(), reynard_host, std::to_string(reynard_port));
+    boost::asio::ip::tcp::resolver::iterator endpoints_iterator= resolver.resolve(resolver_query);
+    if (endpoints_iterator == boost::asio::ip::tcp::resolver::iterator())
+    {
+        std::cerr << "Could not resolve Reynard the Robot host" << std::endl;
+        return 1;
+    }
+    boost::asio::ip::tcp::endpoint reynard_endpoint = *endpoints_iterator;
+    std::cout << "Connecting to Reynard the Robot ASCII socket at " << reynard_endpoint << std::endl;
+
     // Use RobotRaconteur::NodeSetup to initialize Robot Raconteur
-    RR::ServerNodeSetup node_setup(ROBOTRACONTEUR_SERVICE_TYPES, "experimental.reynard_the_robot_cpp_rest", 59201, argc, argv);
+    RR::ServerNodeSetup node_setup(ROBOTRACONTEUR_SERVICE_TYPES, "experimental.reynard_the_robot_cpp_socket", 59201, argc, argv);
 
     // Create the Reynard service instance
-    auto reynard_obj = RR_MAKE_SHARED<Reynard_impl>("localhost", 29201);
+    auto reynard_obj = RR_MAKE_SHARED<Reynard_impl>(reynard_endpoint);
 
     // Register the service with Robot Raconteur
     auto ctx = RR::RobotRaconteurNode::s()->RegisterService("reynard", "experimental.reynard_the_robot", reynard_obj);
