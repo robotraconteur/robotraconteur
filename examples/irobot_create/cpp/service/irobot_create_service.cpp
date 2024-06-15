37caecafbac067cb8de9811d6bac2ec6eaf5e73a
John Wason <wason@wasontech.com>
2024-06-14 23:14:33 -0400

    Use named constants for OI opcodes in C++ iRobot create service

--- examples/irobot_create/cpp/service/irobot_create_service.cpp
+++ examples/irobot_create/cpp/service/irobot_create_service.cpp
@@ -17,6 +17,27 @@ namespace c3 = experimental::create3;
 #define M_PI 3.14159265358979323846
 #endif
 
+#define CREATE_OI_OP_START 128
+#define CREATE_OI_OP_FULL 132
+#define CREATE_OI_OP_DRIVE 137
+#define CREATE_OI_OP_LEDS 139
+#define CREATE_OI_OP_SONG 140
+#define CREATE_OI_OP_DRIVE_DIRECT 145
+#define CREATE_OI_OP_STREAM 148
+#define CREATE_OI_OP_STREAM_RESUME 150
+
+#define CREATE_OI_FLAGS_BUMP_RIGHT 0x1
+#define CREATE_OI_FLAGS_BUMP_LEFT 0x2
+#define CREATE_OI_FLAGS_WHEEL_DROP_RIGHT 0x4
+#define CREATE_OI_FLAGS_WHEEL_DROP_LEFT 0x8
+#define CREATE_OI_FLAGS_WHEEL_DROP_CASTER 0x10
+
+#define CREATE_OI_FLAGS_PLAY_BUTTON 0x1
+#define CREATE_OI_FLAGS_ADVANCE_BUTTON 0x4
+
+#define CREATE_OI_SET_LED_PLAY 0x2
+#define CREATE_OI_SET_LED_ADVANCE 0x8
+
 class Create_impl : public c3::Create_default_impl, public boost::enable_shared_from_this<Create_impl>
 {
   public:
@@ -39,7 +60,8 @@ class Create_impl : public c3::Create_default_impl, public boost::enable_shared_
         serial_port->set_option(boost::asio::serial_port::flow_control());
         serial_port->set_option(boost::asio::serial_port::character_size());
 
-        std::vector<uint8_t> start_cmd = {128, 132, 150, 0, 139, 0, 80, 255};
+        std::vector<uint8_t> start_cmd = {
+            CREATE_OI_OP_START, CREATE_OI_OP_FULL, CREATE_OI_OP_STREAM_RESUME, 0, CREATE_OI_OP_LEDS, 0, 80, 255};
         boost::asio::write(*serial_port, boost::asio::buffer(start_cmd));
 
         boost::this_thread::sleep(boost::posix_time::milliseconds(100));
@@ -54,7 +76,7 @@ class Create_impl : public c3::Create_default_impl, public boost::enable_shared_
 
         boost::mutex::scoped_lock lock(this_lock);
 
-        std::vector<uint8_t> stop_cmd = {128};
+        std::vector<uint8_t> stop_cmd = {CREATE_OI_OP_START};
         boost::asio::write(*serial_port, boost::asio::buffer(stop_cmd));
 
         serial_port->close();
@@ -75,7 +97,7 @@ class Create_impl : public c3::Create_default_impl, public boost::enable_shared_
 
         boost::this_thread::sleep(boost::posix_time::milliseconds(100));
 
-        std::vector<uint8_t> command = {148, 1, 6};
+        std::vector<uint8_t> command = {CREATE_OI_OP_STREAM, 1, 6};
         boost::asio::write(*serial_port, boost::asio::buffer(command));
     }
 
@@ -88,7 +110,7 @@ class Create_impl : public c3::Create_default_impl, public boost::enable_shared_
 
         streaming = false;
 
-        std::vector<uint8_t> command = {150, 0};
+        std::vector<uint8_t> command = {CREATE_OI_OP_STREAM_RESUME, 0};
         boost::asio::write(*serial_port, boost::asio::buffer(command));
     }
 
@@ -103,7 +125,7 @@ class Create_impl : public c3::Create_default_impl, public boost::enable_shared_
         std::stringstream ss;
         cereal::PortableBinaryOutputArchive ar(ss, cereal::PortableBinaryOutputArchive::Options::BigEndian());
         // opcode
-        ar((uint8_t)137);
+        ar((uint8_t)CREATE_OI_OP_DRIVE);
         ar(v);
         ar(r);
 
@@ -122,7 +144,7 @@ class Create_impl : public c3::Create_default_impl, public boost::enable_shared_
         std::stringstream ss;
         cereal::PortableBinaryOutputArchive ar(ss, cereal::PortableBinaryOutputArchive::Options::BigEndian());
         // opcode
-        ar((uint8_t)145);
+        ar((uint8_t)CREATE_OI_OP_DRIVE_DIRECT);
         ar(vr);
         ar(vl);
 
@@ -138,11 +160,11 @@ class Create_impl : public c3::Create_default_impl, public boost::enable_shared_
 
         uint8_t bits = 0;
         if (play.value)
-            bits |= 2;
+            bits |= CREATE_OI_SET_LED_PLAY;
         if (advance.value)
-            bits |= 8;
+            bits |= CREATE_OI_SET_LED_ADVANCE;
 
-        std::vector<uint8_t> command = {139, bits, 80, 255};
+        std::vector<uint8_t> command = {CREATE_OI_OP_LEDS, bits, 80, 255};
         boost::asio::write(*serial_port, boost::asio::buffer(command));
     }
 
@@ -310,23 +332,23 @@ class Create_impl : public c3::Create_default_impl, public boost::enable_shared_
         ar(velocity_left_h);
 
         int32_t state_flags = 0;
-        if (bump_flags_b & 1)
+        if (bump_flags_b & CREATE_OI_FLAGS_BUMP_RIGHT)
         {
             state_flags |= c3::CreateStateFlags::bump_right;
         }
-        if (bump_flags_b & 2)
+        if (bump_flags_b & CREATE_OI_FLAGS_BUMP_LEFT)
         {
             state_flags |= c3::CreateStateFlags::bump_left;
         }
-        if (bump_flags_b & 4)
+        if (bump_flags_b & CREATE_OI_FLAGS_WHEEL_DROP_RIGHT)
         {
             state_flags |= c3::CreateStateFlags::wheel_drop_right;
         }
-        if (bump_flags_b & 8)
+        if (bump_flags_b & CREATE_OI_FLAGS_WHEEL_DROP_LEFT)
         {
             state_flags |= c3::CreateStateFlags::wheel_drop_left;
         }
-        if (bump_flags_b & 0x10)
+        if (bump_flags_b & CREATE_OI_FLAGS_WHEEL_DROP_CASTER)
         {
             state_flags |= c3::CreateStateFlags::wheel_drop_caster;
         }
@@ -358,12 +380,12 @@ class Create_impl : public c3::Create_default_impl, public boost::enable_shared_
             state_flags |= c3::CreateStateFlags::virtual_wall;
         }
 
-        if ((buttons_b & 0x1) != 0)
+        if ((buttons_b & CREATE_OI_FLAGS_PLAY_BUTTON) != 0)
         {
             state_flags |= c3::CreateStateFlags::play_button;
         }
 
-        if ((buttons_b & 0x4) != 0)
+        if ((buttons_b & CREATE_OI_FLAGS_ADVANCE_BUTTON) != 0)
         {
             state_flags |= c3::CreateStateFlags::advance_button;
         }
@@ -403,7 +425,7 @@ class Create_impl : public c3::Create_default_impl, public boost::enable_shared_
             auto cb_func = rrvar_play_callback->GetClientFunction(play_client);
             auto notes = cb_func(rrvar_distance_traveled, rrvar_angle_traveled);
 
-            std::vector<uint8_t> command = {140, 0, (uint8_t)(notes->size() / 2)};
+            std::vector<uint8_t> command = {CREATE_OI_OP_SONG, 0, (uint8_t)(notes->size() / 2)};
             boost::range::copy(*notes, std::back_inserter(command));
             command.push_back(141);
             command.push_back(0);
