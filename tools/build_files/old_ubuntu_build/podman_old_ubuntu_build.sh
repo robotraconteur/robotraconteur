9ed2d06c3facdef4f697e44fa960d7d8cdee5b2e
John Wason <wason@wasontech.com>
2025-01-07 16:11:01 -0500

    Include companion build in old ubuntu build

--- tools/build_files/old_ubuntu_build/podman_old_ubuntu_build.sh
+++ tools/build_files/old_ubuntu_build/podman_old_ubuntu_build.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-if [ ! -f `pwd`/robotraconteur/tools/build_files/old_ubuntu_build/old_ubuntu_build.sh ]; then
+if [ ! -f `pwd`/src/robotraconteur/tools/build_files/old_ubuntu_build/old_ubuntu_build.sh ]; then
     echo "This script must be run from the parent dir of the Robot Raconteur source tree"
     exit 1
 fi
 
-podman run -v `pwd`/robotraconteur:/work/robotraconteur -w /work --rm ubuntu:xenial /work/robotraconteur/tools/build_files/old_ubuntu_build/old_ubuntu_build.sh
+podman run -v `pwd`/src/:/work/src -w /work --rm ubuntu:xenial /work/src/robotraconteur/tools/build_files/old_ubuntu_build/old_ubuntu_build.sh
