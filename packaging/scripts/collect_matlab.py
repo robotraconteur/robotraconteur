50a41b0f70330c4bb23da317d4bf6f8fee77d765
John Wason <wason@wasontech.com>
2025-06-04 03:53:07 -0400

    fix collect_matlab.py

--- packaging/scripts/collect_matlab.py
+++ packaging/scripts/collect_matlab.py
@@ -7,6 +7,8 @@ pathlib.Path("distfiles").mkdir(exist_ok=True)
 
 for asset_dir in asset_dirs:
     for fname in list(pathlib.Path(asset_dir).glob('**/RobotRaconteur-*-MATLAB*')):
+        if fname.is_dir():
+            continue
         print(fname)
         dest = pathlib.Path(fname)
         shutil.copy(str(fname), "distfiles/" + dest.name)
