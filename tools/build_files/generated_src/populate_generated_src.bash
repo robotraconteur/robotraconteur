e5c508321d7b5d90ea4336d0b7c822ea771e81cd
John Wason <wason@wasontech.com>
2025-07-13 14:07:05 -0400

    Fix ROS package paths (#345)
    * Don't use --install-layout=deb for ROS 2
    
    * Don't use --install-layout=deb for ROS 2
    
    * Improve ROS 2 deb package Python installation location
    
    * Add test actions
    
    * Change --install-lib parameter for ROS setup.py
    
    * Add test actions
    
    * Add test actions
    
    * Add test actions
    
    * Add test actions
    
    * Add test actions
    
    * Add test actions
    
    * Add test actions
    
    * Remove debian buster action

--- tools/build_files/generated_src/populate_generated_src.bash
+++ tools/build_files/generated_src/populate_generated_src.bash
@@ -10,7 +10,7 @@ fi
 mkdir -p build_gen_src
 cd build_gen_src
 cmake -DPACKAGE_SWIG_SOURCE_ALL=ON -DBUILD_PYTHON3=ON -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DUSE_PREGENERATED_SOURCE=OFF $CMAKE_ARGS ..
-cmake --build . --config=Release --target package_source_swig
+cmake --build . --config Release --target package_source_swig
 cp -r generated_src ..
 cd ..
 rm -r build_gen_src
