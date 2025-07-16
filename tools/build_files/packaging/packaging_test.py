aafd72fa6318e019431e3069f5b19f9699b2235b
John Wason <wason@wasontech.com>
2025-07-16 02:38:09 -0400

    Add gen debian

--- tools/build_files/packaging/packaging_test.py
+++ tools/build_files/packaging/packaging_test.py
@@ -6,10 +6,205 @@ from pathlib import Path
 import string
 import re
 import os
+from datetime import datetime, timezone
 
 _containers = {
+    "debian": "docker.io/debian:sid",
     "gentoo": "docker.io/gentoo/stage3:latest"
 }
+
+# debian
+
+_debian_control = \
+    """\
+Source: robotraconteur
+Section: contrib/misc
+Priority: optional
+Maintainer: John Wason <wason@wasontech.com>
+Build-Depends: cmake (>=3.5.1),
+               debhelper (>=9),
+               libbluetooth-dev,
+               libboost-all-dev (>=1.58.0),
+               libdbus-1-dev,
+               libssl-dev (>=1),
+               libusb-1.0-0-dev (>=1),
+               zlib1g-dev,
+               python3-dev,
+               python3-numpy,
+               python3-setuptools
+Standards-Version: 4.7.0
+Homepage: https://github.com/robotraconteur/robotraconteur
+
+Package: librobotraconteurcore1
+Architecture: any
+Multi-Arch: same
+Section: contrib/libs
+Depends: ${misc:Depends}, ${shlibs:Depends}, libbluetooth3, libdbus-1-3, libusb-1.0-0
+Pre-Depends: ${misc:Pre-Depends}
+Description: Robot Raconteur is a communication framework for
+ Robotics and Automation
+ .
+ This package provides run-time library of robotraconteur.
+
+Package: librobotraconteur-dev
+Architecture: any
+Section: contrib/libdevel
+Depends: librobotraconteurcore1, ${misc:Depends}, ${shlibs:Depends}, libboost-all-dev (>=1.58.0)
+Description: Robot Raconteur is a communication framework for
+ Robotics and Automation
+ .
+ This package provides development files of robotraconteur.
+
+Package: python3-robotraconteur
+Architecture: any
+Section: contrib/python
+Depends: ${misc:Depends}, ${shlibs:Depends}, libbluetooth3,
+         libdbus-1-3, libusb-1.0-0, python3-numpy, python3
+Description: Robot Raconteur Python module
+ Robot Raconteur Python module. Use with python 3
+
+Package: robotraconteurgen
+Architecture: any
+Section: contrib/libdevel
+Depends: librobotraconteurcore1, ${misc:Depends}, ${shlibs:Depends}
+Description: Robot Raconteur is a communication framework for
+ Robotics and Automation
+ .
+ This package provides the RobotRaconteurGen tool.
+"""
+
+_debian_copyright = \
+    """\
+Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: robotraconteur
+Source: http://github.com/robotraconteur/robotraconteur
+
+Files: *
+Copyright: 2018 John Wason (wason@wasontech.com)
+License: Apache
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+Files: debian/*
+Copyright: 2018 John Wason <wason@wasontech.com>
+License: GPL-2+
+ This package is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ .
+ This package is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ .
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <https://www.gnu.org/licenses/>
+ .
+ On Debian systems, the complete text of the GNU General
+ Public License version 2 can be found in "/usr/share/common-licenses/GPL-2".
+"""
+
+_debian_python3_robotraconteur_install = \
+    """\
+/usr/lib/python3*/dist-packages/RobotRaconteur/*.py
+/usr/lib/python3*/dist-packages/RobotRaconteur/*.so
+/usr/lib/python3*/dist-packages/RobotRaconteur-*.egg-info/*
+"""
+
+_debian_librobotraconteur_dev_install = \
+    """\
+/usr/include/RobotRaconteur.h
+/usr/include/RobotRaconteur/*
+/usr/lib/*/*.so
+/usr/lib/*/cmake/
+"""
+
+_debian_librobotraconteurcore1_install = \
+    """\
+/usr/lib/*/*.so.*
+"""
+
+_debian_robotraconteurgen_install = \
+    """\
+/usr/bin/RobotRaconteurGen
+/usr/share/man/*/robotraconteurgen*
+"""
+
+_debian_changelog = \
+    """
+robotraconteur (${TEMPLATE_VERSION}-${TEMPLATE_PACKAGE_VERSION}${TEMPLATE_DEBIAN_VERSION_SUFFIX}) ${TEMPLATE_DEBIAN_DIST}; urgency=medium
+
+  * Test Build
+
+ -- John Wason <wason@wasontech.com>  ${TEMPLATE_TIMESTAMP}
+"""
+
+_debian_rules = \
+    """\
+#!/usr/bin/make -f
+# See debhelper(7) (uncomment to enable)
+# output every command that modifies files on the build system.
+#export DH_VERBOSE = 1
+
+export DH_OPTIONS
+
+include /usr/share/dpkg/architecture.mk
+
+CPPFLAGS :=$(shell dpkg-buildflags --get CPPFLAGS)
+CFLAGS   :=$(shell dpkg-buildflags --get CFLAGS)
+CXXFLAGS :=$(shell dpkg-buildflags --get CXXFLAGS)
+LDFLAGS  :=$(shell dpkg-buildflags --get LDFLAGS)
+
+export DEB_HOST_MULTIARCH CPPFLAGS CFLAGS CXXFLAGS LDFLAGS
+
+COMMON_CMAKE_FLAGS = \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+	-DLIB_INSTALL_DIR=/usr/lib/$(DEB_HOST_MULTIARCH) \
+    -DCMAKE_LIBRARY_ARCHITECTURE="$(DEB_HOST_MULTIARCH)" \
+    -DBUILD_TESTING=$(if $(filter nocheck,$(DEB_BUILD_OPTIONS)),OFF,ON) \
+    -DBUILD_GEN=ON \
+    -DBUILD_PYTHON=OFF \
+    -DBUILD_PYTHON3=ON \
+    -DUSE_PREGENERATED_SOURCE=ON \
+    -DPREGENERATED_SOURCE_DIR:PATH=$(CURDIR)/generated_src \
+    -DPYTHON3_EXECUTABLE=/usr/bin/python3 \
+    -DINSTALL_PYTHON3=ON \
+    -DSETUPTOOLS_DEB_LAYOUT=ON \
+    -DBUILD_DOCUMENTATION=OFF \
+    -DROBOTRACONTEUR_TESTING_DISABLE_DISCOVERY_LOOPBACK=ON \
+    -DBUILD_SHARED_LIBS=ON \
+    -DROBOTRACONTEURCORE_SOVERSION_MAJOR_ONLY=ON \
+    -DCMAKE_SKIP_RPATH=ON \
+    -DCMAKE_GTEST_DISCOVER_TESTS_DISCOVERY_MODE=PRE_TEST
+
+
+%:
+	dh $@ --parallel
+
+override_dh_auto_configure:
+	dh_auto_configure -- $(COMMON_CMAKE_FLAGS)
+
+override_dh_auto_build:
+	LD_LIBRARY_PATH=$(CURDIR)/obj-$(DEB_HOST_GNU_TYPE)/out/lib:$$LD_LIBRARY_PATH dh_auto_build
+
+override_dh_makeshlibs:
+	LD_LIBRARY_PATH=$(CURDIR)/obj-$(DEB_HOST_GNU_TYPE)/out/lib:$$LD_LIBRARY_PATH dh_makeshlibs -- -c4
+
+override_dh_auto_test:
+	LD_LIBRARY_PATH=$(CURDIR)/obj-$(DEB_HOST_GNU_TYPE)/out/lib:$(CURDIR)/obj-$(DEB_HOST_GNU_TYPE)/test/out/lib:$$LD_LIBRARY_PATH dh_auto_test --no-parallel
+
+"""
+
+
+# gentoo
 
 _gentoo_ebuild = \
     """
@@ -171,6 +366,56 @@ def get_version(src_dir, args_version):
     return config_h_ver_str
 
 
+def do_gen_debian(args):
+    src_dir = get_src_dir(args.src_dir)
+    ver = get_version(src_dir, args.version)
+    print(f"src_dir: {src_dir}")
+    print(f"ver: {ver}")
+    out_dir = get_out_dir(args.out_dir, src_dir)
+    print(f"out_dir: {out_dir}")
+
+    debian_dir = out_dir / "debian"
+    debian_dir.mkdir(parents=True, exist_ok=True)
+    (debian_dir / "source").mkdir(parents=True, exist_ok=True)
+
+    data = {
+        "TEMPLATE_TIMESTAMP": datetime.now(timezone.utc).strftime("%a, %d %b %Y %H:%M:%S +0000"),
+        "TEMPLATE_VERSION": ver,
+        "TEMPLATE_PACKAGE_VERSION": args.package_version or "1",
+        "TEMPLATE_DEBIAN_VERSION_SUFFIX": args.debian_version_suffix,
+        "TEMPLATE_DEBIAN_DIST": args.debian_dist
+    }
+    deb_changelog_filled = string.Template(_debian_changelog).substitute(data)
+
+    deb_files_to_write = {
+        "control": _debian_control,
+        "python3-robotraconteur.install": _debian_python3_robotraconteur_install,
+        "librobotraconteur-dev.install": _debian_librobotraconteur_dev_install,
+        "librobotraconteurcore1.install": _debian_librobotraconteurcore1_install,
+        "robotraconteurgen.install": _debian_robotraconteurgen_install,
+        "changelog": deb_changelog_filled.strip(' \t\n\r') + "\n\n",
+        "rules": _debian_rules,
+        "source/format": "3.0 (quilt)\n",
+        "compat": "10\n",
+        "copyright": _debian_copyright
+    }
+
+    if args.stdout:
+        for k, v in deb_files_to_write.items():
+            print(f"=== debian/{k} ===")
+            print(v)
+            print("")
+    else:
+        for k, v in deb_files_to_write.items():
+            print(f"writing file {str(debian_dir / k)}")
+            with open(debian_dir / k, "w") as f:
+                f.write(v)
+
+            rules_path = debian_dir / "rules"
+            if rules_path.exists():
+                rules_path.chmod(0o755)
+
+
 def do_gen_gentoo(args):
     src_dir = get_src_dir(args.src_dir)
     ver = get_version(src_dir, args.version)
@@ -221,6 +466,8 @@ def do_gen(args):
 
     if args.dist == "gentoo":
         do_gen_gentoo(args)
+    elif args.dist == "debian":
+        do_gen_debian(args)
     else:
         raise Exception(f"Unknown dist {args.dist}")
 
